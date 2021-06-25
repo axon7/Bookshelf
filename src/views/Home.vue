5e8154120b67322c571eed23968ca0fef8b01433
axon7 <kubaz9991@gmail.com>
2021-06-25 22:22:30 +0200

    setup

---
+++ src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <div v-for="item in items" :key="item.id">
+      {{ item.title }}
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue";
+import axios from "axios";
+export default {
+  name: "Home",
+  components: {},
+  data() {
+    return {
+      items: [],
+    };
+  },
+  async created() {
+    let data = await axios.get("https://jsonplaceholder.typicode.com/posts");
+    this.items = data.data;
+  },
+};
+</script>
