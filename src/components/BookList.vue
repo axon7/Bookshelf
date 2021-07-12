dfada917856b4d777c900feaf1be16e8d2ea9d06
axon7 <kubaz9991@gmail.com>
2021-07-12 23:36:40 +0200

    show book img

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,10 +1,12 @@
 <template>
-  <div v-if="books.length > 0">
+  <div class="books-container" v-if="books.length > 0">
     <router-link
+      class="book-card"
       :to="{ name: 'BookView', params: { id: book.id } }"
       v-for="book in books"
       :key="book.id"
     >
+      <img :src="book.volumeInfo.imageLinks.thumbnail" />
       {{ book.volumeInfo.title }}
     </router-link>
   </div>
@@ -21,3 +23,21 @@ export default {
   },
 };
 </script>
+<style scoped lang="scss">
+.books-container {
+  max-width: 700px;
+  margin: 0 auto;
+  padding-left: 15px;
+  padding-right: 15px;
+  width: 100%;
+}
+
+.book-card {
+  display: flex;
+  align-items: center;
+  background-color: grey;
+  margin-bottom: 20px;
+  border-radius: 4px;
+  min-height: 125px;
+}
+</style>
