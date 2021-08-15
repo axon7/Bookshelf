f1fae970eb5ed5dfbf892f3ef0f3a9c01e38ba5b
axon7 <kubaz9991@gmail.com>
2021-08-15 23:19:46 +0200

    fetch book

--- client/src/views/BookView.vue
+++ client/src/views/BookView.vue
@@ -3,8 +3,35 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "BookView",
+  data() {
+    return {
+      book: {},
+    };
+  },
+  mounted() {
+    this.fetchBook();
+    // console.log(this.$route.params.id);
+  },
+  methods: {
+    fetchBook() {
+      console.log("searching");
+      const bookId = this.$route.params.id;
+      console.log(bookId);
+
+      let data = axios
+        .get(`https://www.googleapis.com/books/v1/volumes/${bookId}`)
+        .then((response) => {
+          console.log(response.data);
+          this.book = response.data.volumeInfo;
+          this.loadState = "success";
+        });
+      this.items = data.data;
+    },
+  },
 };
 </script>
 
